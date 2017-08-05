ddb50c4751a8bd6a98a8a892cd0e45d0bfc3b44b
filipmatijevic <30616936+filipmatijevic@users.noreply.github.com>
2017-08-05 12:55:02 +0200

    First Push for Cube behaviour in 3d
    Nothing important

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+	private Renderer cubeColor;
+	private Color Color1;
+	private Color Color2;
+	public float val;
+
+	// Use this for initialization
+	void Start () {
+		Color1 = Color.red;
+		Color2 = Color.blue;
+
+		cubeColor = this.GetComponent<Renderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		val += Time.deltaTime;
+		cubeColor.material.color = Color.Lerp (Color1, Color2, Mathf.PingPong (val, 1f));
+		
+	}
+}
